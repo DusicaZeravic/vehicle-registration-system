d49870ad0d29aaa68deab4b279b723cad54a3657
Dusica Zeravic <dusicazeravic@MacBook-Air.local>
2025-06-25 16:07:25 +0200

    added http loading bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { Router, RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./shared/header/header.component";
 import { CommonModule } from "@angular/common";
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 @Component({
   selector: "app-root",
@@ -9,6 +10,7 @@ import { CommonModule } from "@angular/common";
   imports: [
     RouterOutlet,
     CommonModule,
+    LoadingBarHttpClientModule,
     HeaderComponent
   ],
   templateUrl: "./app.component.html",
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
 import { ToastrModule } from 'ngx-toastr';
+import { importProvidersFrom } from '@angular/core';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
+    importProvidersFrom(
+      LoadingBarModule,
+      LoadingBarHttpClientModule,
+    ),
     ...ToastrModule.forRoot({
       timeOut: 0,
       closeButton: true,
