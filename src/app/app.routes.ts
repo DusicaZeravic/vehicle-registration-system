dc71b79f1cc73dd39e22594d5a50a1419180ac34
Dusica Zeravic <dusicazeravic@MacBook-Air.local>
2025-06-30 12:37:42 +0200

    added not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -26,5 +27,5 @@ export const routes: Routes = [
     redirectTo: "auth/login",
     pathMatch: "full",
   },
-  { path: "**", redirectTo: "auth/login" },
+  { path: '**', component: NotFoundComponent },
 ];
